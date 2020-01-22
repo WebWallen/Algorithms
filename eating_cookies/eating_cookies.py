39ba633f09911fd3052c45f6f99f59cfa8f934f9
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-22 18:57:43 -0500

    Completely wrong implementation of Eating Cookies but at least I learned how to find permutations (which Ithought was the way to go about this but just realized that's not-at-all-right after looking at the print statements closely). Sigh. Will try something else tomorrow.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,24 @@
-#!/usr/bin/python
 
-import sys
 
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n):
+  from itertools import permutations
+  cheat_array = []
+  cheat_array.append(n)
+  combinations = list(permutations(range(1, n)))
+  for combo in combinations:
+    print(combo)
+  print(f"Total Permutations: {len(combinations)}")
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+eating_cookies(3)
+eating_cookies(4)
+eating_cookies(5)
+
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
