1487809ba05f63fac276001420febb53f48e5e68
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-22 19:39:37 -0500

    Finished Cookie Monster. Used the same equation we utilized for Fibonacci with one extra step (n - 3). TBH I don't completely understand why this works but three versus two callbacks seems logical since there are 3 returns within the base cases. Coincidence or direct correlation? You tell me!

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,24 +1,41 @@
+import sys
 
+# Eating cookies take two (think I thought this through with equations used during lecture on my mind)
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
-# recursive solution
 def eating_cookies(n):
+  # There is only one way to eat zero cookies.
+  if n == 0:
+    return 1
+  # If n = 1 (1 way to eat). If n = 2 (2 ways to eat)
+  if n <= 2:
+    return n
+  # N = 20. 19, 18, 17. 18, 17, 16. 17, 16, 15... (three callbacks for each num to match the 3 above?)
+  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
+# I can't explain exactly why this works but it does. Same formula we used for Fibonacci with one extra equation.
+
+print(eating_cookies(3))
+print(eating_cookies(5))
+print(eating_cookies(10))
+print(eating_cookies(100))
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    num_cookies = int(sys.argv[1])
+    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+  else:
+    print('Usage: eating_cookies.py [num_cookies]')
+
+# This is wrong but I'm keeping it just in case it's helpful later
+def permutation_practice(n):
   from itertools import permutations
   cheat_array = []
   cheat_array.append(n)
   combinations = list(permutations(range(1, n)))
-  for combo in combinations:
-    print(combo)
+  # for combo in combinations:
+    # print(combo)
   print(f"Total Permutations: {len(combinations)}")
 
-eating_cookies(3)
-eating_cookies(4)
-eating_cookies(5)
-
-# if __name__ == "__main__":
-#   if len(sys.argv) > 1:
-#     num_cookies = int(sys.argv[1])
-#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-#   else:
-#     print('Usage: eating_cookies.py [num_cookies]')
+permutation_practice(3)
+permutation_practice(4)
+permutation_practice(5)
