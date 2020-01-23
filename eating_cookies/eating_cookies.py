80e5c936af8f3ff833b68ae8b3b5efbeb6c0972b
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-22 19:53:12 -0500

    Code quit working, commented out the problem lines.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -17,20 +17,20 @@ def eating_cookies(n):
 print(eating_cookies(3))
 print(eating_cookies(5))
 print(eating_cookies(10))
-print(eating_cookies(100))
+# print(eating_cookies(100))
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
 
 # This is wrong but I'm keeping it just in case it's helpful later
 def permutation_practice(n):
   from itertools import permutations
-  cheat_array = []
-  cheat_array.append(n)
+  convert_num_to_array = []
+  convert_num_to_array.append(n)
   combinations = list(permutations(range(1, n)))
   # for combo in combinations:
     # print(combo)
