b897d0303a462fe9816c94ba4a7723c1dee73018
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-23 09:34:46 -0500

    Completed extremely naive and unreusable RPS algorithm. Would like to discuss in 1-on-1 and see if there's a way to simplify logic.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,33 @@
 
 import sys
 
+# Rock, paper, scissors = starting array
+
 def rock_paper_scissors(n):
-  pass 
+  from itertools import permutations
+  two_moves = []
+  # three_moves = []
+  # four_moves = []
+  # five_moves = []
+  # six_moves = []
+  if n == 2:
+    two_moves.append("Rock")
+    two_moves.append("Paper")
+    combinations = list(permutations(two_moves))
+    for combo in combinations:
+      print(combo)
+    two_moves.remove("Paper")
+    two_moves.append("Scissors")
+    combinations = list(permutations(two_moves))
+    for combo in combinations:
+      print(combo)
+    two_moves.remove("Rock")
+    two_moves.append("Paper")
+    combinations = list(permutations(two_moves))
+    for combo in combinations:
+      print(combo)
+
+rock_paper_scissors(2)
 
 
 if __name__ == "__main__":
