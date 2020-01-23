60e63a6982f15a7b4225669e3ac1aafe64567ec9
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-23 09:02:03 -0500

    Added cache to Eating Cookies algorithm.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,22 +2,26 @@ import sys
 
 # Eating cookies take two (think I thought this through with equations used during lecture on my mind)
 
-def eating_cookies(n):
+def eating_cookies(n, cache):
   # There is only one way to eat zero cookies.
   if n == 0:
     return 1
   # If n = 1 (1 way to eat). If n = 2 (2 ways to eat)
-  if n <= 2:
+  elif n <= 2:
     return n
+  # Store data in cache for ALL variations of n to save and prevent needless repetition/time complexity
+  elif cache[n] > 0:
+    return cache[n]
+  # Use cache with recursive calls for the same reason. Saves each result to the cache for ease of operations
+  cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
   # N = 20. 19, 18, 17. 18, 17, 16. 17, 16, 15... (three callbacks for each num to match the three returns above?)
-  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+  return cache[n]
 
 # I can't explain exactly why this works but it does. Same formula we used for Fibonacci with one extra equation.
+n = 500
+cache = {i: 0 for i in range (n + 1)}
 
-print(eating_cookies(3))
-print(eating_cookies(5))
-print(eating_cookies(10))
-# print(eating_cookies(100))
+print(eating_cookies(n, cache))
 
 # if __name__ == "__main__":
 #   if len(sys.argv) > 1:
