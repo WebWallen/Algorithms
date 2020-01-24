676c54668eff6be428cd627a89fb80bf3f57e143
Daniel Wallen <daniel.wallen1987@gmail.com>
2020-01-23 19:35:37 -0500

    Completed RPS thanks to a nudge in the right direction by a brainiac in help channel. Wow, I was overcomplicating this one! Also deleted the naive solution.

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -5,29 +5,33 @@ import sys
 # Rock, paper, scissors = starting array
 
 def rock_paper_scissors(n):
-  from itertools import permutations
-  if n == 2:
-    two_moves = ['RP', 'RS', 'RR', 'PR', 'PS', 'PP', 'SP', 'SR', 'SS']
-    for move in two_moves:
-      if move[0] == move[1]:
-        print(move)
-      else: 
-        move = list(permutations(move))
-        print(move)
-  elif n == 3:
-    three_moves = ['RRR', 'RRP', 'RRS', 'RPR', 'RPP', 'RPS', 'RSR', 'RSP', 'RSS', 
-    'PPP', 'PPR', 'PPS', 'PRR', 'PRS', 'PRP', 'PSS', 'PSR', 'PSP', 
-    'SSS', 'SSR', 'SSP', 'SRR', 'SRS', 'SRP', 'SPS', 'SPP', 'SPR']
-    for move in three_moves:
-      if move[0] == move[1] and move[0] == move[2] and move[2] == move[1]:
-        print(move)
-      elif move[0] == move[2]:
-        print(move)
-      else:
-        move = list(permutations(move))
-        print(move)
-rock_paper_scissors(2)
-rock_paper_scissors(3)
+  if n == 0:
+    # No moves left to make, so return an empty array
+    return [[]] 
+  elif n == 1:
+    # There's only 1 variation of each move when n = 1
+    return [['rock'], ['paper'], ['scissors']] # R P S
+  else:
+    # Set up an empty array structure then multiply by three (options) to the nth power (permutations)
+    all_rps_combos = [[]] * 3 ** n # RR RP RS 
+    # Recursive way to calculate every single RPS option for input n
+    rps_combos = rock_paper_scissors(n - 1) # RRR RRP RRS / RRRR RRRP RRRS / RRRRR RRRRP RRRRS / RRRRRR RRRRRP RRRRRS
+    # Note: we need two counters since we have a placeholder array of current combo and a "final" array for total combos
+    j = 0
+    # Initialize for loop and set to length of current combos (needs to run through Rock, Paper, and Scissors)
+    for i in range(len(rps_combos)):
+      all_rps_combos[j] = rps_combos[i] + ['rock']
+      # Increment second index after each pass so we can add the permutation for all three variations
+      j += 1
+      all_rps_combos[j] = rps_combos[i] + ['paper']
+      j += 1
+      all_rps_combos[j] = rps_combos[i] + ['scissors']
+      j += 1
+    # Return total
+    return all_rps_combos
+
+print(rock_paper_scissors(2))
+print(rock_paper_scissors(4))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
